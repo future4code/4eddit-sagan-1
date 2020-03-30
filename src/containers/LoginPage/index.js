be11283a494a76ce517e6fd6b40bdcc0ad01a8c8
marinaisabels <marinaisabels@live.com>
2020-03-30 10:25:49 -0300

    teste

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
+
 class LoginPage extends Component {
   render() {
     return (
       <div>
-        Oie
+        <form>
+        <input type="" placeholder="Email"/>
+        <input type="" placeholder="senha"/>
+        <button>Entrar</button>
+        <button>Cadastrar</button>
+        </form>
       </div>
     );
   }
---
+++ src/containers/RegisterPage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class RegisterPage extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+        <input type="" placeholder="Nome do usuário"/>
+        <input type="" placeholder="Email"/>
+        <input type="" placeholder="senha"/>
+        <button>Cadastrar</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterPage;
